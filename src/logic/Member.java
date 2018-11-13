3b065fd1d9a5afbf48fee59b15011b4695cdccbd
Martin <Martin@10.50.138.86>
2018-11-13 11:47:11 +0100

    tilføjet getters til member class

--- src/logic/Member.java
+++ src/logic/Member.java
@@ -11,4 +11,29 @@ public class Member {
     
     private boolean active;
     private boolean elite;
+    
+    
+    public int getID() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public boolean isActive() {
+        return active;
+    }
+    
+    public boolean isElite() {
+        return elite;
+    }
 }
