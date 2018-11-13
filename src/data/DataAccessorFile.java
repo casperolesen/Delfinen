9e7f05dda6660253bcf530bd917166984c49e303
Martin <Martin@10.50.138.86>
2018-11-13 11:13:27 +0100

    tilføjet DAF

---
+++ src/data/DataAccessorFile.java
@@ -0,0 +1,36 @@
+package data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DataAccessorFile {
+    public List readAllLines(String filename) throws IOException{
+        try {
+            Charset cs = Charset.forName("ISO-8859-1");
+            Path path = Paths.get(filename);
+            List<String> lines = Files.readAllLines(path, cs);
+            
+            return lines;
+        } catch (IOException ex) {
+            throw new IOException();
+        }
+    }
+    
+    public void appendLine(String outFilename, String input) throws IOException{
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter(outFilename, true));
+            out.append(input+System.lineSeparator());
+            out.close();
+        } catch (IOException ex) {
+            throw new IOException();
+        }
+    }
+    
+    
+}
